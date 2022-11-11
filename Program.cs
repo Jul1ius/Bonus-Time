e30161e4c90f90df751c9ff621a2c66a666abea3
Ts. Hristov <42769725+KiwiB0y@users.noreply.github.com>
2022-11-11 13:44:29 +0100

    Fix formatting
    Small change proposal for the formatting, such as remove unnecessary curly braces and lines between conditional blocks

--- Program.cs
+++ Program.cs
@@ -12,22 +12,21 @@ namespace Bonus_Time
             if (num <= 100)
                 bonus = 5;
 
-
             else if (num > 100 && num < 1000)
                 bonus = (num * 0.2);
 
             else if (num > 1000)
                 bonus = (num * 0.1);
 
+            
             if (num % 2 == 0)
                 bonus = (bonus + 1);
 
             else if (num % 10 == 5)
                 bonus = (bonus + 2);
-            {
-                Console.WriteLine(bonus);
-                Console.WriteLine(num + bonus);
-            }
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(num + bonus);
         }
     }
 }
